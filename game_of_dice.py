648089fc15e97c2744d67a08024db4aeade3be0d
aryansharma <121270238+aryansharma1709@users.noreply.github.com>
2023-05-02 19:43:28 +0530

    Update game_of_dice.py

--- game_of_dice.py
+++ game_of_dice.py
@@ -1,6 +1,8 @@
-import random as a
-print('WELCOME TO THE GAME OF DICE!!')
-print('YOU ARE ROLL UP THE TWO DICES')
-x=a.randint(1,6)
-y=a.randint(1,6)
-print(f''' OUTPUT: ({x},{y})''')
+import random
+ls=[]
+print('WELCOME TO YOUR ONLINE DICE ROLL!')
+n=int(input("Enter the number of dice you want to roll at a time: "))
+for i in range(n):
+    a=random.randint(1,6)
+    ls.append(a)
+print(ls) 
