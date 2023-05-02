d5a42e6bf01c69bbb56ac74c9f8885cac706542a
aryansharma <121270238+aryansharma1709@users.noreply.github.com>
2023-05-02 19:40:31 +0530

    Update otp_generator.py

--- otp_generator.py
+++ otp_generator.py
@@ -1,5 +1,14 @@
-import random as a
-print("Welcome to OTP Generator!!")
-print("OTP no. generated")
-out=a.randint(100000,999999)
-print(out)
+import string
+import random
+num=string.digits
+alpha_num=string.digits+string.ascii_letters
+print("OTP GENERATOR!")
+c=input("Enter 'an' for alphanumeric OTP or 'n' for OTP of digits only: ")
+if c=='an':
+    otp_ls=random.choices(alpha_num,k=6)
+    otp=''.join(otp_ls)
+    print(otp)
+else:
+    otp_ls=random.choices(num,k=6)
+    otp=''.join(otp_ls)
+    print(otp)
